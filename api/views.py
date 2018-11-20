6919e069defa8a14acd5f4c64a5da3d79ebad23d
Alexey Sokolov <alexey@ebi.ac.uk>
2018-11-20 16:20:58 +0000

    Fixing bugs

--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from elasticsearch import Elasticsearch
-import json
 from django.views.decorators.csrf import csrf_exempt
+import json
+
 
 STAGING_NODE1 = 'wp-np3-e2:9200'
 STAGING_NODE2 = 'wp-np3-e3:9200'
@@ -13,11 +14,27 @@ def index(request, name):
         return HttpResponse("This method is not allowed!\n")
     if name != 'file' and name != 'organism' and name != 'specimen' and name != 'dataset':
         return HttpResponse("This method is not allowed!\n")
-    size = request.GET.get('size', 100000)
+    size = request.GET.get('size', 10)
     es_staging = Elasticsearch([STAGING_NODE1, STAGING_NODE2])
     if request.body:
-        results = es_staging.search(index=name, body=json.loads(request.body), size=size)
+        results = es_staging.search(index=name, size=size, body=json.loads(request.body))
     else:
-        results = es_staging.search(index=name, size=size)
+        field = request.GET.get('_source', '')
+        sort = request.GET.get('sort', '')
+        query = request.GET.get('q', '')
+        if query != '':
+            results = es_staging.search(index=name, size=size, _source=field, sort=sort, q=query)
+        else:
+            results = es_staging.search(index=name, size=size, _source=field, sort=sort)
+    results = json.dumps(results)
+    return HttpResponse(results)
+
+
+@csrf_exempt
+def detail(request, name, id):
+    if request.method != 'GET':
+        return HttpResponse("This method is not allowed!\n")
+    es_staging = Elasticsearch([STAGING_NODE1, STAGING_NODE2])
+    results = es_staging.search(index=name, q="_id:{}".format(id))
     results = json.dumps(results)
     return HttpResponse(results)
--- faang_proxy/settings.py
+++ faang_proxy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(mmyqo94dlfc25abr(upys!(ftly8_bg#-9wq%#36&bo@0#&5*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
