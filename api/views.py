4545808cff30cc3e6a1065d040639d04674c44e4
Alexey Sokolov <alexey@ebi.ac.uk>
2018-12-13 14:28:51 +0000

    Fixing bug with json file

--- api/views.py
+++ api/views.py
@@ -14,7 +14,7 @@ def index(request, name):
     size = request.GET.get('size', 10)
     es = Elasticsearch([settings.NODE1, settings.NODE2])
     if request.body:
-        results = es.search(index=name, size=size, body=json.loads(request.body))
+        results = es.search(index=name, size=size, body=json.loads(request.body.decode("utf-8")))
     else:
         field = request.GET.get('_source', '')
         sort = request.GET.get('sort', '')
@@ -49,16 +49,22 @@ def get_protocols(request):
     for result in results["hits"]["hits"]:
         if "specimenFromOrganism" in result["_source"]:
             key = result['_source']['specimenFromOrganism']['specimenCollectionProtocol']['filename']
+            try:
+                protocol_type = result['_source']['specimenFromOrganism']['specimenCollectionProtocol']['url'].split("/")[5]
+            except:
+                protocol_type = ""
             parsed = key.split("_")
             if parsed[0] in settings.UNIVERSITIES:
                 name = settings.UNIVERSITIES[parsed[0]]
                 protocol_name = " ".join(parsed[2:-1])
                 date = parsed[-1].split(".")[0]
-                entries.setdefault(key, {"name": "", "date": "", "protocol_name": "", "key": ""})
+                entries.setdefault(key, {"name": "", "date": "", "protocol_name": "", "key": "", "protocol_type": ""})
                 entries[key]['university_name'] = name
-                entries[key]['protocol_date'] = date
+                entries[key]['protocol_date'] = date[0:4]
                 entries[key]["protocol_name"] = protocol_name
                 entries[key]["key"] = key
+                if protocol_type in ["analysis", "assays", "samples"]:
+                    entries[key]["protocol_type"] = protocol_type
     results = json.dumps(list(entries.values()))
     return HttpResponse(results)
 
