7fe41432ef2c94a2669957bb2ca80b137a956908
Alexey Sokolov <alexey@ebi.ac.uk>
2019-08-30 09:22:02 +0100

    Adding new indeces to whitelist

--- api/views.py
+++ api/views.py
@@ -6,8 +6,10 @@ from django.core.cache import cache
 import json
 
 
-ALLOWED_INDICES = ['file', 'organism', 'specimen', 'dataset', 'experiment', 'protocol_files', 'protocol_samples',
-                   'article', 'protocol_analysis', 'analysis']
+ALLOWED_INDICES = ['file', 'organism', 'specimen', 'dataset', 'experiment',
+                   'protocol_files', 'protocol_samples', 'article',
+                   'protocol_analysis', 'analysis', 'summary_organism',
+                   'summary_specimen', 'summary_dataset', 'summary_file']
 
 
 @csrf_exempt
@@ -27,21 +29,24 @@ def index(request, name):
     data = None
 
     # Get cache if request goes to file or specimen
-    if int(size) == 100000 and query == '':
-        cache_key = "{}_key".format(name)
-        cache_time = 86400
-        data = cache.get(cache_key)
-        set_cache = True
+    # if int(size) == 100000 and query == '':
+    #     cache_key = "{}_key".format(name)
+    #     cache_time = 86400
+    #     data = cache.get(cache_key)
+    #     set_cache = True
 
     if not data:
         es = Elasticsearch([settings.NODE1, settings.NODE2])
         if request.body:
-            data = es.search(index=name, size=size, body=json.loads(request.body.decode("utf-8")))
+            data = es.search(index=name, size=size, body=json.loads(
+                request.body.decode("utf-8")))
         else:
             if query != '':
-                data = es.search(index=name, size=size, _source=field, sort=sort, q=query)
+                data = es.search(index=name, size=size, _source=field,
+                                 sort=sort, q=query)
             else:
-                data = es.search(index=name, size=size, _source=field, sort=sort)
+                data = es.search(index=name, size=size, _source=field,
+                                 sort=sort)
         if set_cache:
             cache.set(cache_key, data, cache_time)
 
@@ -52,8 +57,11 @@ def index(request, name):
 def detail(request, name, id):
     if request.method != 'GET':
         return HttpResponse("This method is not allowed!\n")
+    if name not in ALLOWED_INDICES:
+        return HttpResponse("This index doesn't exist!\n")
     es = Elasticsearch([settings.NODE1, settings.NODE2])
     results = es.search(index=name, q="_id:{}".format(id))
     if results['hits']['total'] == 0:
-        results = es.search(index=name, q="alternativeId:{}".format(id), doc_type="_doc")
+        results = es.search(index=name, q="alternativeId:{}".format(id),
+                            doc_type="_doc")
     return JsonResponse(results)
