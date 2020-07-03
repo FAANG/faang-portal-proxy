3f7c3dc704b0e237fc654729ea99a32596981537
Alexey Sokolov <alexey@ebi.ac.uk>
2020-07-03 12:37:36 +0100

    Fixing thr issues

--- api/urls.py
+++ api/urls.py
@@ -5,9 +5,12 @@ from . import views
 urlpatterns = [
     path('<str:name>/_search/', views.index, name='index'),
     path('<str:name>/<str:id>', views.detail, name='detail'),
-    path('fire_api/trackhubregistry/{str:genome_id}/<str:folder>/<str:doc_id>',
+    path('fire_api/trackhubregistry/<str:genome_id>/<str:folder>/<str:doc_id>',
          views.trackhubregistry_with_dirs_fire_api,
          name='trackhubregistry_with_dirs_fire_api'),
+    path('fire_api/trackhubregistry/<str:genome_id>/<str:doc_id>',
+         views.trackhubregistry_with_dir_fire_api,
+         name='trackhubregistry_with_dir_fire_api'),
     path('fire_api/trackhubregistry/<str:doc_id>',
          views.trackhubregistry_fire_api, name='trackhubregistry_fire_api'),
     path('fire_api/<str:protocol_type>/<str:id>', views.protocols_fire_api,
